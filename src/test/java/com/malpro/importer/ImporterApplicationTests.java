1763954ac20a2b3961f12433c5de9316f076e6c6
Martin Fahian <martin.fahian@embedit.cz>
2022-10-27 10:12:24 +0200

    Initial commit

---
+++ src/test/java/com/malpro/importer/ImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malpro.importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
